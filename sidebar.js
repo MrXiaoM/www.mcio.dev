00b449a4d0c93fcdcf966acf3e188985b05f4d22
人間工作 <coolxiaom95@gmail.com>
2025-02-03 18:40:42 +0800

    initial commit

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+/**
+ * 在这个文件储存自定义的侧边栏结构
+ * https://ecosystem.vuejs.press/zh/themes/default/config.html#sidebar
+ */
+const customSidebar = [
+    ["/rules", "玩家协议/服务器规则"],
+]
+
+export default customSidebar
